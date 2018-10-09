5ab93ed9c8ec3a492e4fed73bc30fa70ddb78652
SardineFish <Sardine@live.cn>
2018-10-10 00:06:54 +0800

    Fix bugs of list diff

--- test/diffTest.js
+++ test/diffTest.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const lib_js_1 = require("../build/lib.js");
-let ans = lib_js_1.diff([1, 2, 3, 4, 5, 6, 7, 8, 7, 0, 9, 9], [0, 2, 3, 4, 5, 6, 6, 6, 8, 0, 0, 0, 0, 0, 0, 0]);
+let ans = lib_js_1.diff([0, 1, 2, 3], [1, 2, 3, 4]);
 console.log(ans);
 //# sourceMappingURL=diffTest.js.map
--- test/diffTest.ts
+++ test/diffTest.ts
@@ -1,4 +1,4 @@
 import { diff } from "../build/lib.js";
 
-let ans = diff([1, 2, 3, 4, 5, 6, 7, 8, 7,0,9,9], [0, 2, 3, 4, 5, 6, 6, 6, 8,0,0,0,0,0,0,0]);
+let ans = diff([0,1,2,3], [1,2,3,4]);
 console.log(ans);
